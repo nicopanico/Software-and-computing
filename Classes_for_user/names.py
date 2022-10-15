6775cc8b182a6e08f456955c369a4f04148fb8fc
nicopanico <nicopanico@hotmail.it>
2022-10-15 19:21:34 +0200

    ...normal update..

--- Classes_for_user/names.py
+++ Classes_for_user/names.py
@@ -12,17 +12,21 @@ Dataclass to define all the names used for the dataset columns
 """
 @dataclass(frozen=True)
 class names:
-    inizio="DATA INIZIO"
-    fine="DATA FINE"
-    esito="DATA ESITO"
-    accettazione="DATA ACCETTAZIONE"
-    ID="ID PER"
+    inizio="DATA_INIZIO"
+    fine="DATA_FINE"
+    esito="DATA_ESITO"
+    accettazione="DATA_ACCETTAZIONE"
+    ID="ID_PER"
     setting="SETTING"
     deceduto="DECEDUTO"
-    sesso="PER KEY SESSO"
-    malattia="MALATTIA IN CORSO"
+    sesso="PER_KEY_SESSO"
+    malattia="MALATTIA_IN_CORSO"
+    descrizione_esenzione="Descrizione_Esenzione"
+
+@dataclass(frozen=True)
+class keywords:
+    decesso='DECESSO'
     esito="ESITO"
-    descrizione_esenzione="Descrizione esenzione"
     
     
     
