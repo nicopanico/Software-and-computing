474d0852d8df43fec6e01595b977fe61fd60f40e
nicopanico <nicopanico@hotmail.it>
2022-10-23 19:59:23 +0200

    complete testing git add .! rememebr to fix the testing for the fucntion create target id list at line 45, the assert is not correct

--- test_functions.py
+++ test_functions.py
@@ -4,7 +4,7 @@ Created on Sat Sep 17 11:42:15 2022
 
 @author: nicop
 """
-import my_functions
+import my_functions as ff
 import pandas as pd
 import hypothesis
 from hypothesis import strategies as st
@@ -12,17 +12,21 @@ from hypothesis import settings
 from hypothesis import given
 from hypothesis.extra.pandas import data_frames, columns, column
 import pandera as pa
-import names
 from Classes_for_user import names,sett_hosp,patology
 
 
 
 
-df=data_frames(columns=columns(["SETTING","ID"],dtype=str),
+
+@given(df=data_frames(columns=columns(["SETTING","ID"],dtype=str),
                          rows=st.tuples(
                              st.from_regex("TERAPIA INTENSIVA\ (COVID|NO COVID)",fullmatch=True),
-                             st.from_regex("PAT\-[0-9]",fullmatch=True))).example()
-def test_create_target_ID_list(df=df,key_word=sett_hosp.hospital.intensiva_covid,col_name=names.names.setting):
+                             st.from_regex("PAT\-[0-9]",fullmatch=True))),key_word=st.from_regex("TERAPIA INTENSIVA COVID",fullmatch=True),col_name=st.from_regex("SETTING", fullmatch=True))
+
+       
+@settings(max_examples = 5)
+
+def test_create_target_ID_list(df,key_word,col_name):
     """
     Test that given a certain dataset with random rows the fucntion append in the list only the rows with the wanted 
     Key word
@@ -37,15 +41,16 @@ def test_create_target_ID_list(df=df,key_word=sett_hosp.hospital.intensiva_covid
         nothing happens if you search for a key_word that does not exist in the column_name.....your list will be len==0
     """
     
-    test_list=my_functions.create_target_ID_list(df, key_word, col_name)
+    test_list=ff.create_target_ID_list(df, key_word, col_name)
     assert len(test_list)==df[col_name].value_counts()[key_word], f"expected the same number"
     
 
    
-df=data_frames(columns=columns(["DECEDUTO"],dtype=str),
+@given(df=data_frames(columns=columns(["DECEDUTO"],dtype=str),
                         rows=st.tuples(
-                             st.integers(0,1))).example()
-def test_deceased_list(df=df):
+                             st.integers(0,1))))
+@settings(max_examples = 5)
+def test_deceased_list(df):
     """
     Test that given a dataset containing a deceased column the fucntion appends in the colums only the rows that have
     1 as value for deceased
@@ -54,16 +59,26 @@ def test_deceased_list(df=df):
     As output:
         list containing the row numbers of the deceased patients
     """
-    isdead=my_functions.deceased_list(df)
+    isdead=ff.deceased_list(df)
     assert len(isdead)==df.loc[df.DECEDUTO == 1, 'DECEDUTO'].count(), f"expected the same number"
-     
 
+
+     
+@given(list1=st.lists(st.integers()),
+list2=st.lists(st.integers()))
+@settings(max_examples = 5)
 
 def test_common_elements(list1,list2):
     """
     test if the fucntion is correcting removing common elements of the 2 lists from list1
     """   
+    outputList=ff.common_elements(list1, list2)
+    #assert that the final list has elements from list1 that are not in list 2
+    #First convert lists into set
+    set2=set(list2)
+    assert any(x in set2  for x in outputList)==False, f"expected the output list and the list2 to not share elements"
     
+        
                         
     
     
